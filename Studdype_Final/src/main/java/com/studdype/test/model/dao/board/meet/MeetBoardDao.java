4341974fc43f0293ae9a75a98cb78aa8287bd52f
chosuk1212 <chosuk0000@gmail.com>
2020-12-30 21:11:27 +0900

    re pull

--- Studdype_Final/src/main/java/com/studdype/test/model/dao/board/meet/MeetBoardDao.java
+++ Studdype_Final/src/main/java/com/studdype/test/model/dao/board/meet/MeetBoardDao.java
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-package com.studdype.test.model.dao.board.meet;
 
-public interface MeetBoardDao {
-	
-	//모임 게시판 게시글 
-	String NAMESPACE="meetboard.";
-	
-}
-=======
 /*
  * package com.studdype.test.model.dao.board.meet;
  * 
@@ -18,7 +9,7 @@ public interface MeetBoardDao {
  * 
  * public interface MeetBoardDao {
  * 
- * //모임 게시판 게시글 String NAMESPACE="meetboard.";
+ * //紐⑥엫 寃뚯떆�뙋 寃뚯떆湲� String NAMESPACE="meetboard.";
  * 
  * public List<MeetDto> meet_selectList(); public MeetDto meet_selectOne(int
  * meet_no); public int meet_insert(MeetDto meet_dto); public int
@@ -26,4 +17,4 @@ public interface MeetBoardDao {
  * 
  * }
  */
->>>>>>> 5bf1b79b42db84f7bd726a9887b431be39617a02
+
