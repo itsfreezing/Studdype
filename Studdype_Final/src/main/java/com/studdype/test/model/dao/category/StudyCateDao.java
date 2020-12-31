ebf7aa0af5f404f4c10bcde8513a380c08ead31b
kimo9561 <kimo9561@naver.com>
2021-01-01 00:55:31 +0900

    createStuddype 관련 사항 1. createStuddype.jsp - jsp 페이지 생성
    2. HomeController
    - createStuddype.jsp 연결 매퍼 추가
    - StudyBiz autowired 추가
    
    3. createStuddype.css
    - css 파일 추가
    
    4. pom.xml
    - file upload 관련 dependency 추가
    
    5. applicationContext.xml
    - fileload bean 추가
    
    6. studdypeHome.jsp
    - 스터디 생성 a태그 수정
    
    7. studdypeHeader.jsp
    - 스터디 생성 a태그 수정
    
    8. StduyBiz / Impl
    - Si/Gu, category list 메소드 추가
    - Impl : 각 dao autowired 추가
    
    9. LocationSiDao / Impl
    - Si list 메소드 추가
    - Impl : SqlSessionTemplate 추가
    
    10. LocationGuDao / Impl
    - Gu list 메소드 추가.
    - Impl : SqlSessionTemplate 추가
    
    11. config.xml
    - Location 시/구 , category 별칭 추가
    - Location 시/구, category mapper 경로 설정
    
    12. locationSi-mapper.xml 추가
    - locationSiList (select) 추가
    
    13. locationGu-mapper.xml 추가
    - locationGuList(select) 추가
    
    14. StudyCateDao
    - categoryList 추가
    - Impl :  SqlSessionTemplate 추가
    
    15. category-mapper.xml
    - categoryList(select) 추가

--- Studdype_Final/src/main/java/com/studdype/test/model/dao/category/StudyCateDao.java
+++ Studdype_Final/src/main/java/com/studdype/test/model/dao/category/StudyCateDao.java
@@ -1,6 +1,12 @@
 package com.studdype.test.model.dao.category;
 
+import java.util.List;
+
+import com.studdype.test.model.dto.study.StudyCategoryDto;
+
 public interface StudyCateDao {
 	//스터디 카테고리 
 	String NAMESPACE="studycate.";
+	
+	public List<StudyCategoryDto> categoryList();
 }
--- Studdype_Final/src/main/java/com/studdype/test/model/dao/category/StudyCateDaoImpl.java
+++ Studdype_Final/src/main/java/com/studdype/test/model/dao/category/StudyCateDaoImpl.java
@@ -1,8 +1,31 @@
 package com.studdype.test.model.dao.category;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.studdype.test.model.dto.study.StudyCategoryDto;
 
 @Repository
 public class StudyCateDaoImpl implements StudyCateDao{
+
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	@Override
+	public List<StudyCategoryDto> categoryList() {
+		List<StudyCategoryDto> list = new ArrayList<StudyCategoryDto>();
+		
+		try {
+			list = sqlSession.selectList(NAMESPACE+"categoryList");
+		} catch (Exception e) {
+			System.out.println("[ERROR] : categoryList");
+			e.printStackTrace();
+		}
+		return list;
+	}
 
 }
